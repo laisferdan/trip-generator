c229ce665ed4bdf9e889785cee53d88a8d8459f3
Laís Fernandes Daniel <laisfd@hotmail.com>
2024-03-15 08:52:17 -0300

    initial commit

---
+++ src/main/java/personal/tripgenerator/TripGeneratorApplication.java
@@ -0,0 +1,13 @@
+package personal.tripgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personal/tripgenerator/TripGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.tripgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
